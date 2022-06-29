cf26b0920de1568977a200e59eca74e1320324a3
Brijisha <brijishadoshi12@gmail.com>
2022-06-29 12:09:57 +0530

    Setup backend -express server and Mongodb databse via mogoose package.

---
+++ index.js
@@ -0,0 +1,15 @@
+const conectToMongo = require("./db");
+const express = require("express");
+
+conectToMongo();
+
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
